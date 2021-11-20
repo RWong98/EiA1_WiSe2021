ad60b9c1191be6a3ddce38da05aec4457d0d3450
RWongMKB <92439984+RWong98@users.noreply.github.com>
2021-11-20 21:43:51 +0100

    Warm Up Exercises 3 and 6

---
+++ Exercises/warm-up-exercise-6/warm-up-exercise-6.ts
@@ -0,0 +1,20 @@
+var anzahl: number = 0;
+
+function buttonClicked() {
+anzahl++;
+document.querySelector("#result").innerHTML = anzahl + "";
+}
+
+function buttonClicked1() {
+    document.querySelector("#button1").innerHTML = ("Der Zeh.");
+    document.querySelector("#button1").addEventListener("click", buttonClicked1)
+}
+
+
+window.addEventListener("load", function() {
+
+
+document.querySelector("#button2").addEventListener("click", buttonClicked);
+
+
+})
