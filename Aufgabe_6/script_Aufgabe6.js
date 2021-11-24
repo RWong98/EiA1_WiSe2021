bf2092798615ee919a24ca42190afcece3ec7676
RWongMKB <92439984+RWong98@users.noreply.github.com>
2021-11-24 15:36:58 +0100

    Aufgabe 6 Update
    Relative Emissionen funktionieren jetzt, Reihenfolge des Scripts geändert. (Tipp im Tutorium bei Christian Müllerschön.)
    Relative Rechnungen und WW2018 jetzt nach all den anderen Rechnungen.

--- Aufgabe_6/script_Aufgabe6.js
+++ Aufgabe_6/script_Aufgabe6.js
@@ -1,6 +1,4 @@
 window.addEventListener("load", function () {
-    /*Worldwide Rechnung*/
-    var WW2018 = Math.round(EU2018 + AF2018 + SA2018 + NA2018 + AS2018 + AU2018);
     /*Kontinente Variablen*/
     var EU = "Europa";
     var AF = "Africa";
@@ -12,46 +10,54 @@ window.addEventListener("load", function () {
     var EU2008 = 4965.7;
     var EU2018 = 4209.3;
     /*Europe Rechnungen*/
-    var EUrelative = Math.round((100 / WW2018) * EU2018);
-    var EUzuEUP = Math.round((EU2018 / EU2008 - 1) * 100);
-    var EUzuEUA = Math.round(EU2018 - EU2008);
+    var EUzuEUP = (EU2018 / EU2008 - 1) * 100;
+    var EUzuEUA = EU2018 - EU2008;
     /*Africa Werte*/
     var AF2008 = 1028;
     var AF2018 = 1235.5;
     /*Africa Rechnungen*/
-    var AFrelative = Math.round((100 / WW2018) * AF2018);
-    var AFzuAFP = Math.round((AF2018 / AF2008 - 1) * 100);
-    var AFzuAFA = Math.round(AF2018 - AF2008);
+    var AFzuAFP = (AF2018 / AF2008 - 1) * 100;
+    var AFzuAFA = AF2018 - AF2008;
     /*South America Werte*/
     var SA2008 = 1132.6;
     var SA2018 = 1261.5;
     /*South America Rechnungen*/
-    var SArelative = Math.round((100 / WW2018) * SA2018);
-    var SAzuSAP = Math.round((SA2018 / SA2008 - 1) * 100);
-    var SAzuSAA = Math.round(SA2018 - SA2008);
+    var SAzuSAP = (SA2018 / SA2008 - 1) * 100;
+    var SAzuSAA = SA2018 - SA2008;
     /*North America Werte*/
     var NA2008 = 6600.4;
     var NA2018 = 6035.6;
     /*North America Rechnungen*/
-    var NArelative = Math.round((100 / WW2018) * NA2018);
-    var NAzuNAP = Math.round((NA2018 / NA2008 - 1) * 100);
-    var NAzuNAA = Math.round(NA2018 - NA2008);
+    var NAzuNAP = (NA2018 / NA2008 - 1) * 100;
+    var NAzuNAA = NA2018 - NA2008;
     /*Asia Werte*/
     var AS2008 = 12954.7;
     var AS2018 = 16274.1;
     /*Asia Rechnungen*/
-    var ASrelative = Math.round((100 / WW2018) * AS2018);
-    var ASzuASP = Math.round((AS2018 / AS2008 - 1) * 100);
-    var ASzuASA = Math.round(AS2018 - AS2008);
+    var ASzuASP = (AS2018 / AS2008 - 1) * 100;
+    var ASzuASA = AS2018 - AS2008;
     /*Australia Werte*/
     var AU2008 = 1993;
     var AU2018 = 2100.5;
     /*Australia Rechnungen*/
-    var AUrelative = Math.round((100 / WW2018) * AU2018);
-    var AUzuAuP = Math.round((AU2018 / AU2008 - 1) * 100);
-    var AUzuAUA = Math.round(AU2018 - AU2008);
-    /*Chart*/
-    var chart = document.querySelector(".chart");
+    var AUzuAuP = (AU2018 / AU2008 - 1) * 100;
+    var AUzuAUA = AU2018 - AU2008;
+    /*Worldwide Rechnung*/
+    var WW2018 = EU2018 + AF2018 + SA2018 + NA2018 + AS2018 + AU2018;
+    /*Relative Rechnungne zur Welt, weil sonst von der Reihenfolge nicht gerechnet werden kann*/
+    /*EU*/
+    var EUrelative = (100 / WW2018) * EU2018;
+    console.log((100 / WW2018) * AU2018);
+    /*AF*/
+    var AFrelative = (100 / WW2018) * AF2018;
+    /*SA*/
+    var SArelative = (100 / WW2018) * SA2018;
+    /*NA*/
+    var NArelative = (100 / WW2018) * NA2018;
+    /*AS*/
+    var ASrelative = (100 / WW2018) * AS2018;
+    /*AU*/
+    var AUrelative = (100 / WW2018) * AU2018;
     function continents(continent, continent18, continentzuwelt, continentzucontinent, continentzucontinentco) {
         document.querySelector("#titleRegion").innerHTML = continent;
         document.querySelector("#regionSpecific").innerHTML = continent;
@@ -59,6 +65,7 @@ window.addEventListener("load", function () {
         document.querySelector(".EmissionRelative").innerHTML = continentzuwelt.toFixed(2) + "%";
         document.querySelector(".GrowthPercent").innerHTML = continentzucontinent.toFixed(2) + "%";
         document.querySelector(".GrowthAbsolute").innerHTML = continentzucontinentco.toFixed(2);
+        document.querySelector(".chart").setAttribute("style", "height: 20px");
     }
     document.querySelector(".europe").addEventListener("click", function () { continents(EU, EU2018, EUrelative, EUzuEUP, EUzuEUA); });
     document.querySelector(".africa").addEventListener("click", function () { continents(AF, AF2018, AFrelative, AFzuAFP, AFzuAFA); });
--- Aufgabe_6/script_Aufgabe6.ts
+++ Aufgabe_6/script_Aufgabe6.ts
@@ -1,8 +1,5 @@
 window.addEventListener("load", function(){
 
- /*Worldwide Rechnung*/
- var WW2018: number = Math.round(EU2018 + AF2018 + SA2018 + NA2018 + AS2018 + AU2018);
-
  /*Kontinente Variablen*/
  var EU: string = "Europa";
  var AF: string = "Africa";
@@ -15,53 +12,61 @@ window.addEventListener("load", function(){
  var EU2008: number = 4965.7;
  var EU2018: number = 4209.3;
  /*Europe Rechnungen*/
- var EUrelative: number = Math.round((100 / WW2018) * EU2018);
- var EUzuEUP: number = Math.round((EU2018 / EU2008 - 1) * 100);
- var EUzuEUA: number = Math.round(EU2018 - EU2008);
+ var EUzuEUP: number = (EU2018 / EU2008 - 1) * 100;
+ var EUzuEUA: number = EU2018 - EU2008;
 
  /*Africa Werte*/
  var AF2008: number = 1028;
  var AF2018: number = 1235.5;
  /*Africa Rechnungen*/
- var AFrelative: number = Math.round((100 / WW2018) * AF2018);
- var AFzuAFP: number = Math.round((AF2018 / AF2008 - 1) * 100);
- var AFzuAFA: number = Math.round(AF2018 - AF2008);
+ var AFzuAFP: number = (AF2018 / AF2008 - 1) * 100;
+ var AFzuAFA: number = AF2018 - AF2008;
  
  /*South America Werte*/
  var SA2008: number = 1132.6;
  var SA2018: number = 1261.5;
  /*South America Rechnungen*/
- var SArelative: number = Math.round((100 / WW2018) * SA2018);
- var SAzuSAP: number = Math.round((SA2018 / SA2008 - 1) * 100);
- var SAzuSAA: number = Math.round(SA2018 - SA2008);
+ var SAzuSAP: number = (SA2018 / SA2008 - 1) * 100;
+ var SAzuSAA: number = SA2018 - SA2008;
 
  /*North America Werte*/
  var NA2008: number = 6600.4;
  var NA2018: number = 6035.6;
  /*North America Rechnungen*/
- var NArelative: number = Math.round((100 / WW2018) * NA2018);
- var NAzuNAP: number = Math.round((NA2018 / NA2008 - 1) * 100);
- var NAzuNAA: number = Math.round(NA2018 - NA2008);
+ var NAzuNAP: number = (NA2018 / NA2008 - 1) * 100;
+ var NAzuNAA: number = NA2018 - NA2008;
 
  /*Asia Werte*/
  var AS2008: number = 12954.7;
  var AS2018: number = 16274.1;
  /*Asia Rechnungen*/
- var ASrelative: number = Math.round((100 / WW2018) * AS2018);
- var ASzuASP: number = Math.round((AS2018 / AS2008 - 1) * 100);
- var ASzuASA: number = Math.round(AS2018 - AS2008);
+ var ASzuASP: number = (AS2018 / AS2008 - 1) * 100;
+ var ASzuASA: number = AS2018 - AS2008;
 
  /*Australia Werte*/
  var AU2008: number = 1993;
  var AU2018: number = 2100.5;
  /*Australia Rechnungen*/
- var AUrelative: number = Math.round((100 / WW2018) * AU2018);
- var AUzuAuP: number = Math.round((AU2018 / AU2008 - 1) * 100);
- var AUzuAUA: number = Math.round(AU2018 - AU2008);
+ var AUzuAuP: number = (AU2018 / AU2008 - 1) * 100;
+ var AUzuAUA: number = AU2018 - AU2008;
 
- /*Chart*/
- var chart: any = document.querySelector(".chart");
+  /*Worldwide Rechnung*/
+  var WW2018: number = EU2018 + AF2018 + SA2018 + NA2018 + AS2018 + AU2018;
 
+ /*Relative Rechnungne zur Welt, weil sonst von der Reihenfolge nicht gerechnet werden kann*/
+ /*EU*/
+ var EUrelative: number = (100 / WW2018) * EU2018;
+ console.log((100 / WW2018) * AU2018);
+ /*AF*/
+ var AFrelative: number = (100 / WW2018) * AF2018;
+ /*SA*/
+ var SArelative: number = (100 / WW2018) * SA2018;
+ /*NA*/
+ var NArelative: number = (100 / WW2018) * NA2018;
+ /*AS*/
+ var ASrelative: number = (100 / WW2018) * AS2018;
+ /*AU*/
+ var AUrelative: number = (100 / WW2018) * AU2018;
 
 function continents(continent, continent18, continentzuwelt, continentzucontinent, continentzucontinentco) {
     document.querySelector("#titleRegion").innerHTML = continent;
@@ -70,6 +75,7 @@ function continents(continent, continent18, continentzuwelt, continentzucontinen
     document.querySelector(".EmissionRelative").innerHTML = continentzuwelt.toFixed(2) + "%";
     document.querySelector(".GrowthPercent").innerHTML = continentzucontinent.toFixed(2) + "%";
     document.querySelector(".GrowthAbsolute").innerHTML = continentzucontinentco.toFixed(2);
+    document.querySelector(".chart").setAttribute("style", "height: 20px")
 }
 document.querySelector(".europe").addEventListener("click", function(){continents (EU, EU2018, EUrelative, EUzuEUP, EUzuEUA) })
 document.querySelector(".africa").addEventListener("click", function(){continents (AF, AF2018, AFrelative, AFzuAFP, AFzuAFA) })
@@ -77,4 +83,5 @@ document.querySelector(".southamerica").addEventListener("click", function(){con
 document.querySelector(".northamerica").addEventListener("click", function(){continents (NA, NA2018, NArelative, NAzuNAP, NAzuNAA) })
 document.querySelector(".asia").addEventListener("click", function(){continents (AS, AS2018, ASrelative, ASzuASP, ASzuASA) })
 document.querySelector(".australia").addEventListener("click", function(){continents (AU, AU2018, AUrelative, AUzuAuP, AUzuAUA) })
+
 });
