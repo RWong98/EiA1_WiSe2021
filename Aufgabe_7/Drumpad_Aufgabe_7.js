95121acb30df00794451f0a6f8a98afc6c5b5d03
RWongMKB <92439984+RWong98@users.noreply.github.com>
2021-11-25 17:39:36 +0100

    Aufgabe 7 Basics Struktur

---
+++ Aufgabe_7/Drumpad_Aufgabe_7.js
@@ -0,0 +1,7 @@
+var sound = new Audio('sample.mp3');
+sound.play();
+var sound = new Audio('sample.mp3');
+sound.play();
+function playSample(button1, button2, button3) {
+}
+//# sourceMappingURL=Drumpad_Aufgabe_7.js.map
---
+++ Aufgabe_7/Drumpad_Aufgabe_7.ts
@@ -0,0 +1,8 @@
+var sound:HTMLAudioElement = new Audio('sample.mp3');
+sound.play();
+var sound:HTMLAudioElement = new Audio('sample.mp3');
+sound.play();
+
+function playSample(button1, button2, button3){
+
+}
