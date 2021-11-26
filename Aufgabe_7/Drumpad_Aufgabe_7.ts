938461229d8c9344cb0dd1d595fb8da6e32dca56
RWongMKB <92439984+RWong98@users.noreply.github.com>
2021-11-26 02:10:55 +0100

    Aufgabe 7 Update Basics

--- Aufgabe_7/Drumpad_Aufgabe_7.ts
+++ Aufgabe_7/Drumpad_Aufgabe_7.ts
@@ -1,7 +1,5 @@
-var sound:HTMLAudioElement = new Audio('sample.mp3');
-sound.play();
-var sound:HTMLAudioElement = new Audio('sample.mp3');
-sound.play();
+var soundA:HTMLAudioElement = new Audio("A.mp3");
+soundA.play();
 
 function playSample(button1, button2, button3){
 
