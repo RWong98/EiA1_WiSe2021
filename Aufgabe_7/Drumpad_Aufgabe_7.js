5b8bab411b32c5f6cd9dc49de55a8352bdb3ea16
RWongMKB <92439984+RWong98@users.noreply.github.com>
2021-11-27 19:04:03 +0100

    Aufgabe 7 Update Sounds added
    9 Buttons sounds hinzugefügt

--- Aufgabe_7/Drumpad_Aufgabe_7.js
+++ Aufgabe_7/Drumpad_Aufgabe_7.js
@@ -1,7 +1,19 @@
-var sound = new Audio('sample.mp3');
-sound.play();
-var sound = new Audio('sample.mp3');
-sound.play();
-function playSample(button1, button2, button3) {
+/*Array*/
+var sounds = [new Audio("/assets/kick.mp3"), new Audio("/assets/hihat.mp3"), new Audio("/assets/snare.mp3"), new Audio("/assets/A.mp3"), new Audio("/assets/C.mp3"), new Audio("/assets/F.mp3"), new Audio("/assets/G.mp3"), new Audio("/assets/laugh-2.mp3"), new Audio("/assets/ShortFartSound.mp3")];
+document.querySelector("#Button01").addEventListener("click", function () { (sounds[0]).play(); });
+document.querySelector("#Button02").addEventListener("click", function () { (sounds[1]).play(); });
+document.querySelector("#Button03").addEventListener("click", function () { (sounds[2]).play(); });
+document.querySelector("#Button04").addEventListener("click", function () { (sounds[3]).play(); });
+document.querySelector("#Button05").addEventListener("click", function () { (sounds[4]).play(); });
+document.querySelector("#Button06").addEventListener("click", function () { (sounds[5]).play(); });
+document.querySelector("#Button07").addEventListener("click", function () { (sounds[6]).play(); });
+document.querySelector("#Button08").addEventListener("click", function () { (sounds[7]).play(); });
+document.querySelector("#Button09").addEventListener("click", function () { (sounds[8]).play(); });
+function playSample(soundeffects) {
+    var sounds = new Audio(soundeffects);
+    sounds.play();
 }
+setInterval(function () {
+    // Anweisungen
+}, 500);
 //# sourceMappingURL=Drumpad_Aufgabe_7.js.map
--- Aufgabe_7/Drumpad_Aufgabe_7.ts
+++ Aufgabe_7/Drumpad_Aufgabe_7.ts
@@ -1,6 +1,22 @@
-var soundA:HTMLAudioElement = new Audio("A.mp3");
-soundA.play();
+/*Array*/
+var sounds: HTMLAudioElement [] = [new Audio ("/assets/kick.mp3"), new Audio ("/assets/hihat.mp3"), new Audio ("/assets/snare.mp3"), new Audio ("/assets/A.mp3"), new Audio ("/assets/C.mp3"), new Audio ("/assets/F.mp3"), new Audio ("/assets/G.mp3"), new Audio ("/assets/laugh-2.mp3"), new Audio ("/assets/ShortFartSound.mp3")];
 
-function playSample(button1, button2, button3){
+document.querySelector("#Button01").addEventListener("click", function () { (sounds[0]).play(); });
+document.querySelector("#Button02").addEventListener("click", function () { (sounds[1]).play(); });
+document.querySelector("#Button03").addEventListener("click", function () { (sounds[2]).play(); });
+document.querySelector("#Button04").addEventListener("click", function () { (sounds[3]).play(); });
+document.querySelector("#Button05").addEventListener("click", function () { (sounds[4]).play(); });
+document.querySelector("#Button06").addEventListener("click", function () { (sounds[5]).play(); });
+document.querySelector("#Button07").addEventListener("click", function () { (sounds[6]).play(); });
+document.querySelector("#Button08").addEventListener("click", function () { (sounds[7]).play(); });
+document.querySelector("#Button09").addEventListener("click", function () { (sounds[8]).play(); });
 
+
+function playSample(soundeffects){
+    var sounds = new Audio (soundeffects);
+    sounds.play();
 }
+
+setInterval(function() {
+    // Anweisungen
+ }, 500);
